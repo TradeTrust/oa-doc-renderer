b4f2cac55a69a78872d1fc65660d26fb4206d278
Raymond Yeh <yehjxraymond@gmail.com>
2019-07-09 15:45:21 +0800

    Updates

---
+++ precache-manifest.c47a9ffcd8049fa0f225164511df503f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b1f4e09d109202d1e7002635ff95c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7827784e6ec01b8b6b3a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4b1eb46cf2092aed5e38",
+    "url": "/static/css/main.3805ec85.chunk.css"
+  },
+  {
+    "revision": "7827784e6ec01b8b6b3a",
+    "url": "/static/js/2.7f0d09e1.chunk.js"
+  },
+  {
+    "revision": "4b1eb46cf2092aed5e38",
+    "url": "/static/js/main.b71bd431.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
