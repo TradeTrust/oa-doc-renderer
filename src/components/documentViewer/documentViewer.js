41a17d1778f9c70785e079e91f1cf6cbbaf2cb85
samuellye98 <43282419+samuellye98@users.noreply.github.com>
2019-07-12 16:18:45 +0800

    Update documentViewer.js

--- src/components/documentViewer/documentViewer.js
+++ src/components/documentViewer/documentViewer.js
@@ -6,7 +6,6 @@ const DocumentViewer = props => {
   const { tabIndex, document, handleHeightUpdate } = props;
   const templates = documentTemplates(document, handleHeightUpdate);
   const Template = templates[tabIndex].template;
-  handleHeightUpdate();
 
   return <Template document={document} />;
 };
